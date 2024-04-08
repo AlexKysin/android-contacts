2b5d67146e5d68a7cedda5b9303ff6041dc53646
Alexey <alexkysin@gmail.com>
2024-04-08 21:00:42 +0700

    1

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
-public interface ListDiffInterface <T> {
-    public <T> boolean theSameAs(T t);
-    public equals(Object obj);
-    
+public interface ListDiffInterface<T> {
+    public boolean theSameAs(T t);
+    public boolean equals (Object obj);
+
 }
